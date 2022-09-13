f13444b4a2fffc914be139fec39db0288345d9db
Miguel Ayala <miguel.guadarramaayala@mail.mcgill.ca>
2022-09-12 21:06:35 -0400

    Bundle functions

--- SolitonsJulia/tangentBundle.jl
+++ SolitonsJulia/tangentBundle.jl
@@ -1,13 +1,5 @@
 
-"""
-f_bundle(x)
-
-Tangent bundle vector field
-```
-"""
-
-
-function F_bundle!(f, v, p)
+function F_bundle!(v, p)
 
     @unpack E, A, s = p   
     v₁, v₂ = eachcomponent(v)
@@ -22,38 +14,32 @@ function F_bundle!(f, v, p)
 end
 
 
-function Df_bundle!(Df, v, p)
+function G_bundle!(v, p)
 
-    @unpack E, A, s = p
+    @unpack E, A, s = p   
+    v₁, v₂ = eachcomponent(v)
+    
+    ### Cos2t Fourier Series
+    cos2t = Sequence(Fourier(2, 1.0), [.5 0 0 0 .5])
 
-    v₁, v₂, v₃ = eachcomponent(v)
-    project!(component(Df, 1, 1), Multiplication(-σ*one(v₁)))
-    project!(component(Df, 1, 2), Multiplication(σ*one(v₂)))
-    project!(component(Df, 2, 1), Multiplication(ρ-v₃))
-    project!(component(Df, 2, 2), Multiplication(-one(v₂)))
-    return Df
+    project!(component(f, 1),  - v₂ )
+    project!(component(f, 2), + E*v₁ - A*(v₁*cos2t))
+    
+    return G
 end
 
 
-function F_DF!(F, DF, x, σ, ρ, β)
-    γ, u = x[1], component(x, 2)
-    DF .= 0
+function B!(v, p)
 
-    F[1] =
-        (sum(component(u, 1)) - 10.205222700615433) * 24.600655549587863 +
-        (sum(component(u, 2)) - 11.899530531689562) * (-2.4927169722923335) +
-        (sum(component(u, 3)) - 27.000586375896557) * 71.81142025024573
-        component(component(DF, 1, 2), 1)[1,:] .= 24.600655549587863
-        component(component(DF, 1, 2), 2)[1,:] .= -2.4927169722923335
-        component(component(DF, 1, 2), 3)[1,:] .= 71.81142025024573
+    @unpack E, A, s = p   
+    v₁, v₂ = eachcomponent(v)
+    
+    ### Cos2t Fourier Series
+    cos2t = Sequence(Fourier(2, 1.0), [.5 0 0 0 .5])
 
-    project!(component(F, 2), γ * f!(component(F, 2), u, σ, ρ, β) - differentiate(u))
-    f!(component(DF, 2, 1), u, σ, ρ, β)
-    project!(component(DF, 2, 2), γ * Df!(component(DF, 2, 2), u, σ, ρ, β) - Derivative(1))
+    project!(component(f, 1),  - v₂ )
+    project!(component(f, 2), + E*v₁ - A*(v₁*cos2t))
+    
+    return F
 
-    return F, DF
 end
-
-n = 400
-x̄ = Sequence(ParameterSpace() × Fourier(n, 1.0)^3, zeros(ComplexF64, 1+3*(2n+1)))
-newton!((F, DF, x) -> F_DF!(F, DF, x, σ, ρ, β), x̄)
