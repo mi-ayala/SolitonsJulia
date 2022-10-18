7eb63ea5513e80aa016f0d261d4220c5e9e93d0f
Miguel Ayala <miguel.guadarramaayala@mail.mcgill.ca>
2022-10-17 23:44:23 -0400

    Update

--- main.jl
+++ main.jl
@@ -2,7 +2,7 @@ using .SolitonsJulia
 using RadiiPolynomial, UnPack
 
 ### Parameters
-N_F = 20
+N_F = 3
 A = -3
 E = 1
 para = soliton_parameters()
@@ -11,15 +11,19 @@ para = soliton_parameters()
 λ, v = get_bundle(N_F, A, E)
 
 ### Manifold initial guess
-N_T = 20
+N_T = 2
 s_mani = (Fourier(N_F, 1.0) ⊗ Taylor(N_T))^4
 
 ### Periodic orbit data
-P = Sequence(s_mani, ones(ComplexF64, dimension(s_mani)))
+P = Sequence(s_mani, zeros(ComplexF64, dimension(s_mani)))
 
-γ = project(
-    Sequence(Fourier(2, 1.0)^4, [zeros(5) ; zeros(5) ; [.5, 0, 0, 0, .5] ; -2 * [0.5im, 0, 0, 0, -0.5im]]),
-    space(v))
+#  γ = project(
+#      Sequence(Fourier(2, 1.0)^4, [zeros(5) ; zeros(5) ; [.5, 0, 0, 0, .5] ; -2 * [0.5im, 0, 0, 0, -0.5im]]),
+#      space(v))
+
+ γ = project(
+     Sequence(Fourier(2, 1.0)^4, [zeros(5) ; zeros(5) ; [.5, 0, 0, 0, .5] ; -2 * [0.5im, 0, 0, 0, -0.5im]]),
+     space(v))
 
 for i in 1:4
     component(P, i)[(:,0)] .= component(γ, i)
@@ -29,8 +33,6 @@ end
 ### Tapes 
 DF_ = LinearOperator(s_mani, s_mani, zeros(ComplexF64, 4*(2*N_F+1)*(N_T+1), 4*(2*N_F+1)*(N_T+1)))
 F_ = Sequence(s_mani, zeros(ComplexF64, dimension(s_mani)))
-
-
 
 ### Vector field manifold
 function f(P, parameters, N_F, N_T)
@@ -60,7 +62,7 @@ function Df!(Df, P, parameters)
     project!(component(Df, 1, 3), Multiplication(zero(P₃)))
     project!(component(Df, 1, 4), Multiplication(zero(P₄)))
 
-    project!(component(Df, 2, 1), Multiplication(A*P₃ + s*P₁*P₁))
+    project!(component(Df, 2, 1), Multiplication(A*P₃ + s*P₁*P₁-E))
     project!(component(Df, 2, 2), Multiplication(zero(P₂)))
     project!(component(Df, 2, 3), Multiplication(A*P₁))
     project!(component(Df, 2, 4), Multiplication(zero(P₄)))
@@ -79,7 +81,7 @@ function Df!(Df, P, parameters)
 end
 
 
-function F!(F, P, para, N_F, N_T,v)
+function F!(F, P, para, N_F, N_T,v,λ)
 
     project!(F, -f(P, para, N_F, N_T) + Derivative(1,0)*P)
 
@@ -97,32 +99,33 @@ function F!(F, P, para, N_F, N_T,v)
 end
 
 
-    function DF!(DF, P, para, N_F, N_T)
+    function DF!(DF, P, para, N_F, N_T,λ)
   
         DF .= 0
         Df = DF
 
         Df = Df!(Df, P, para)
         
-        project!(component(DF, 1, 1), component(Df, 1, 1) + Derivative(1,0) + λ*project(Derivative(0,1), domain(component(Df, 1, 1)), domain(component(Df,1,1)), ComplexF64))
-        project!(component(DF, 1, 2), component(Df, 1,2))
-        project!(component(DF, 1, 3), component(Df, 1,3))
-        project!(component(DF, 1, 4), component(Df, 1,4))
-        project!(component(DF, 2, 1), component(Df, 2,1))
-        project!(component(DF, 2, 2), component(Df, 2,2) + Derivative(1,0) + λ*project(Derivative(0,1), domain(component(Df, 2, 2)), domain(component(Df,1,1)), ComplexF64))
-        project!(component(DF, 2, 3), component(Df, 2,3))
-        project!(component(DF, 2, 4), component(Df, 2,4))
-        project!(component(DF, 3, 1), component(Df, 3,1))
-        project!(component(DF, 3, 2), component(Df, 3,2))
-        project!(component(DF, 3, 3), component(Df, 3,3) + Derivative(1,0) + λ*project(Derivative(0,1), domain(component(Df, 3, 3)), domain(component(Df,1,1)), ComplexF64))
-        project!(component(DF, 3, 4), component(Df, 3,4))
-        project!(component(DF, 4, 1), component(Df, 4,1))
-        project!(component(DF, 4, 2), component(Df, 4,2))
-        project!(component(DF, 4, 3), component(Df, 4,3))
-        project!(component(DF, 4, 4), component(Df, 4,4) + Derivative(1,0) + λ*project(Derivative(0,1), domain(component(Df, 4, 4)), domain(component(Df,1,1)), ComplexF64))
+        project!(component(DF, 1, 1), -component(Df, 1, 1) + Derivative(1,0) + λ*project(Derivative(0,1), domain(component(Df, 1, 1)), domain(component(Df,1,1)), ComplexF64))
+        project!(component(DF, 1, 2), -component(Df, 1,2))
+        project!(component(DF, 1, 3), -component(Df, 1,3))
+        project!(component(DF, 1, 4), -component(Df, 1,4))
+        project!(component(DF, 2, 1), -component(Df, 2,1))
+        project!(component(DF, 2, 2), -component(Df, 2,2) + Derivative(1,0) + λ*project(Derivative(0,1), domain(component(Df, 2, 2)), domain(component(Df,2,2)), ComplexF64))
+        project!(component(DF, 2, 3), -component(Df, 2,3))
+        project!(component(DF, 2, 4), -component(Df, 2,4))
+        project!(component(DF, 3, 1), -component(Df, 3,1))
+        project!(component(DF, 3, 2), -component(Df, 3,2))
+        project!(component(DF, 3, 3), -component(Df, 3,3) + Derivative(1,0) + λ*project(Derivative(0,1), domain(component(Df, 3, 3)), domain(component(Df,3,3)), ComplexF64))
+        project!(component(DF, 3, 4), -component(Df, 3,4))
+        project!(component(DF, 4, 1), -component(Df, 4,1))
+        project!(component(DF, 4, 2), -component(Df, 4,2))
+        project!(component(DF, 4, 3), -component(Df, 4,3))
+        project!(component(DF, 4, 4), -component(Df, 4,4) + Derivative(1,0) + λ*project(Derivative(0,1), domain(component(Df, 4, 4)), domain(component(Df,4,4)), ComplexF64))
 
-        for i = 1:4
-            component(DF,i,i)[(:,0),(:,0)] = I(2*N_F+1)
+         for i = 1:4
+             component(DF,i,i)[(:,0),(:,0)] = I(2*N_F+1)
+             component(DF,i,i)[(:,1),(:,1)] = I(2*N_F+1)
         end
 
         return DF
@@ -130,7 +133,7 @@ end
 
 
 ### Testing
-     manifold = F!(F_, P, para, N_F, N_T, v)
+      F!(F_, P, para, N_F, N_T, v,λ)
 
-    # DF!(DF_, P, para, N_F, N_T)
-    newton!((F_, DF_, x) -> (F!(F_, x, para, N_F, N_T,v), DF!(DF_, x, para, N_F, N_T)), P)
+#     DF!(DF_, P, para, N_F, N_T,λ)
+#    x_0 = newton!((F_, DF_, x) -> (F!(F_, x, para, N_F, N_T,v,λ), DF!(DF_, x, para, N_F, N_T,λ)), P)
---
+++ test.jl
@@ -0,0 +1,27 @@
+### Parameters
+using RadiiPolynomial
+
+N_F = 1
+N_T = 1
+space = (Fourier(N_F, 1.0) ⊗ Taylor(N_T))^2
+
+x = Sequence(space, zeros(ComplexF64, dimension(space)))
+
+x₁ = component(x,1)
+
+#### Fourier series for Taylor coefficient a0
+x₁[(:,0)]
+
+A = LinearOperator(space, space, zeros(ComplexF64, 2*(2*N_F+1)*(N_T+1), 2*(2*N_F+1)*(N_T+1)))
+
+A₁ = component(A,1,1)
+
+### Derivative first variable with respect to its variables
+A₁.= rand(6,6)
+
+
+### Taylor 0 - Taylor -0 / First fourier column
+component(A,1,1)[(:,0),(-1,0)]
+
+### Taylor 0 - Taylor -0 / First fourier row
+component(A,1,1)[(-1,0),(:,0)]
