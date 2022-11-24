6788be6b458c179f0e76ebe0e923b4d0647e316d
Miguel Ayala <miguel.guadarramaayala@mail.mcgill.ca>
2022-11-24 17:54:29 -0500

    Update

--- main.jl
+++ main.jl
@@ -2,139 +2,84 @@ using SolitonsJulia
 using RadiiPolynomial
 using DifferentialEquations
 using JLD2
+using ForwardDiff
 
 ### Finding Solitons - Shooting
 
 ### Modes
-N_F = 35
-N_T = 30
+N_F = 20
+N_T = 12
 
 
 ### Parameters
 parameters = soliton_parameters()
 
 
-# ### Bundle
-# λ, v = get_bundle(N_F, parameters)
+### Bundle
+λ, v = get_bundle(N_F, parameters)
 
 
-# ### Periodic Orbit
-# γ = project(
-#     Sequence(Fourier(2, 1.0)^4, [zeros(5); zeros(5); [0.5, 0, 0, 0, 0.5]; -2 * [0.5im, 0, 0, 0, -0.5im]]),
-#     space(v))
+### Periodic Orbit
+γ = project(
+    Sequence(Fourier(2, 1.0)^4, [zeros(5); zeros(5); [0.5, 0, 0, 0, 0.5]; -2 * [0.5im, 0, 0, 0, -0.5im]]),
+    space(v))
 
 
-# ### Manifold 
-# P = get_manifold(N_F, N_T, parameters, λ, v, γ );
+# # # ### Manifold 
+ P = get_manifold(N_F, N_T, parameters, λ, v, γ )
 
-# 	file = jldopen("Manifold.jld2");
-# 	P = file["P"];
+# # # # 	file = jldopen("Manifold.jld2");
+# # # # 	P = file["P"];
 
-# jldsave("Manifold.jld2"; P)
+# # # # jldsave("Manifold.jld2"; P)
 
-file = jldopen("Manifold.jld2");
-P = file["P"];
+# # # # file = jldopen("Manifold.jld2");
+# # # # P = file["P"];
 
-N = 10000
+#   N = 100
 
-### Manifold points
-t_range = range(0, stop=2π, length = N)
+# # # # ### Manifold points
+#  t_range = range(0, stop=2π, length = N)
 
 
- σ_range = -1:1
- σ = 1
+# # # #  σ_range = -1:1
+#     σ = 1
 
- u₀= []
+#     u₀= []
  
-for t ∈ t_range 
+#    for t ∈ t_range 
 
-    push!(u₀ , [real(component(P, 1)(t, σ)[(0,0)]), real(component(P, 2)(t, σ)[(0,0)]), real(component(P, 3)(t, σ)[(0,0)]), real(component(P, 4)(t, σ)[(0,0)])])
+#        push!(u₀ , [real(component(P, 1)(t, σ)[(0,0)]), real(component(P, 2)(t, σ)[(0,0)]), real(component(P, 3)(t, σ)[(0,0)]), real(component(P, 4)(t, σ)[(0,0)])])
 
-end
+#    end
     
-
-### Finding
-function condition(u,t,integrator)
-    u[2]
- end
-  
-initial_condition = []  
-bump = []
+# using Plots
 
 
-for i ∈  1:N
+# for i ∈  1:N
 
-    u₀[i]
+#     u₀[i]
 
-    function affect!(integrator) 
-        terminate!(integrator)
-    end
+#     tspan = (0, 10)
+#     prob = ODEProblem(vectorField!,u₀[i],tspan,parameters)
+#     sol = solve(prob, VCABM(),abstol = 1e-14, reltol = 1e-14)
 
-    cb = ContinuousCallback(condition,affect!)
+#     fig = plot(sol.t[:] , sol[1,:])
 
-    tspan = -(0, 10)
-    prob = ODEProblem(vectorField!,u₀[i],tspan,parameters)
-    sol = solve(prob, VCABM(),abstol = 1e-14, reltol = 1e-14, callback=cb)
-    push!(bump, sol[end] )
-
-end
-
-
-for i ∈  1:N
-
-    if abs(bump[i][4]) < 1e-3
-        push!(initial_condition, u₀[i] )
-    end    
-
-end
-
-
-
-function search(x,  parameters)
-	
-    σ = x[1]
-    L = x[2]
-    t = 1
-    
-    u0 = [real(component(P, 1)(t, σ)[(0,0)]), real(component(P, 2)(t, σ)[(0,0)]), real(component(P, 3)(t, σ)[(0,0)]), real(component(P, 4)(t, σ)[(0,0)])]
-
-   tspan = -(0, L)
-   prob = ODEProblem(vectorField!,u0,tspan,parameters)
-   sol = solve(prob, VCABM(),abstol = 1e-13, reltol = 1e-14)
-   out = [sol[end][2], sol[end][4] ]
-
-    return out
-      
-end
-
-DF = x -> ForwardDiff.jacobian(x -> search(x,parameters) , [.5 1])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# σ = -.9
-
- 
-# for t ∈ t_range 
-
-#     push!(u₀ , [real(component(P, 1)(t, σ)[(0,0)]), real(component(P, 2)(t, σ)[(0,0)]), real(component(P, 3)(t, σ)[(0,0)]), real(component(P, 4)(t, σ)[(0,0)])])
+#     display(fig)
 
 # end
-    
+
+
+
+ # ### Finding
+#   function condition(u,t,integrator)
+#       u[2]
+#    end
   
-# p=0
+#  initial_condition = []  
+#  bump = []
+
 
 # for i ∈  1:N
 
@@ -146,7 +91,7 @@ DF = x -> ForwardDiff.jacobian(x -> search(x,parameters) , [.5 1])
 
 #     cb = ContinuousCallback(condition,affect!)
 
-#     tspan = -(0, 5)
+#     tspan = -(0, 10)
 #     prob = ODEProblem(vectorField!,u₀[i],tspan,parameters)
 #     sol = solve(prob, VCABM(),abstol = 1e-14, reltol = 1e-14, callback=cb)
 #     push!(bump, sol[end] )
@@ -156,26 +101,140 @@ DF = x -> ForwardDiff.jacobian(x -> search(x,parameters) , [.5 1])
 
 # for i ∈  1:N
 
-#     if abs(bump[i][4]) < 1e-4
+#     if abs(bump[i][4]) < 1e-3
 #         push!(initial_condition, u₀[i] )
 #     end    
 
 # end
 
 
-# ### Plots
-# tspan = -(0, 4)
-# prob = ODEProblem(vectorField!,initial_condition[1],tspan,p)
-# sol = solve(prob, VCABM(),abstol = 1e-14, reltol = 1e-14)
-# fig = plot(sol.t[:] , sol[1,:])
+# # function search(x,  parameters)
+	
+# #     σ = x[1]
+# #     L = x[2]
+# #     t = -.9
+    
+# #     u0 = [real(component(P, 1)(t, σ)[(0,0)]), real(component(P, 2)(t, σ)[(0,0)]), real(component(P, 3)(t, σ)[(0,0)]), real(component(P, 4)(t, σ)[(0,0)])]
+
+# #    tspan = -(0, L)
+# #    prob = ODEProblem(vectorField!,u0,tspan,parameters)
+# #    sol = solve(prob, VCABM(),abstol = 1e-13, reltol = 1e-14)
+# #    out = [sol[end][2], sol[end][4] ]
+
+# #     return out
+      
+# # end
+
+# # # DF = x0 -> ForwardDiff.jacobian(x -> search(x,parameters) , x0)
+
+# # N = 200
+# # L_N = 200
+# # L_range = range(1, stop=25, length = L_N)
+# # theta_range = range(0, stop=2π, length = N)
+# # data_points = zeros(L_N,3, N)
 
 
-# # for i ∈  2:length(initial_condition)
+# # for i ∈ 1:N 
 
-# #     tspan1 = -(0, 5)
-# #     prob1 = ODEProblem(vectorField!,initial_condition[i],tspan1,p)
-# #     sol1 = solve(prob1, VCABM(),abstol = 1e-13, reltol = 1e-13)
+# #     for j ∈ 1:L_N
+
+# #         dist = norm(search([theta_range[i]  L_range[j] ],  parameters))
+# #         data_points[j,:,i] = [theta_range[i]  L_range[j]  dist ]
+
+# #     end
+
+# # end    
+
+# # global Filter_data = [0 0 0]
+
+
+# # for i ∈ 1:N 
+
+# #     for j ∈ 1:L_N
+
+# #         if data_points[j,3,i] < 10
+
+# #             Filter_data = [Filter_data; transpose(data_points[j,:,i])]
+# #         end    
+
+# #     end
+
+# # end 
+
+# # using GLMakie
+
+# # function plot_peaks_function(x,y,z)
  
-# #     plot!(fig, sol1.t[:] , sol1[1,:])
+# #     fig = Figure(resolution=(900, 900))
+# #     axs = Axis3(fig) 
+# #     surface!(axs, x, y, z,
+# #     colormap = :plasma)
+# #     axs.xlabel = "θ"
+# #     axs.ylabel = "L"
+# #     axs.zlabel = "Norm (u2,u4)"
+
+# #     fig[1,1] = axs
+# #     fig
+# # end
+
+
+# # plot_peaks_function( Filter_data[:,1], Filter_data[:,2], Filter_data[:,3])
+
+
+
+
+# # σ = -.9
+
+ 
+# # for t ∈ t_range 
+
+# #     push!(u₀ , [real(component(P, 1)(t, σ)[(0,0)]), real(component(P, 2)(t, σ)[(0,0)]), real(component(P, 3)(t, σ)[(0,0)]), real(component(P, 4)(t, σ)[(0,0)])])
 
 # # end
+    
+  
+# # p=0
+
+# # for i ∈  1:N
+
+# #     u₀[i]
+
+# #     function affect!(integrator) 
+# #         terminate!(integrator)
+# #     end
+
+# #     cb = ContinuousCallback(condition,affect!)
+
+# #     tspan = -(0, 5)
+# #     prob = ODEProblem(vectorField!,u₀[i],tspan,parameters)
+# #     sol = solve(prob, VCABM(),abstol = 1e-14, reltol = 1e-14, callback=cb)
+# #     push!(bump, sol[end] )
+
+# # end
+
+
+# # for i ∈  1:N
+
+# #     if abs(bump[i][4]) < 1e-4
+# #         push!(initial_condition, u₀[i] )
+# #     end    
+
+# # end
+
+
+# # ### Plots
+# # tspan = -(0, 4)
+# # prob = ODEProblem(vectorField!,initial_condition[1],tspan,p)
+# # sol = solve(prob, VCABM(),abstol = 1e-14, reltol = 1e-14)
+# # fig = plot(sol.t[:] , sol[1,:])
+
+
+# # # for i ∈  2:length(initial_condition)
+
+# # #     tspan1 = -(0, 5)
+# # #     prob1 = ODEProblem(vectorField!,initial_condition[i],tspan1,p)
+# # #     sol1 = solve(prob1, VCABM(),abstol = 1e-13, reltol = 1e-13)
+ 
+# # #     plot!(fig, sol1.t[:] , sol1[1,:])
+
+# # # end
