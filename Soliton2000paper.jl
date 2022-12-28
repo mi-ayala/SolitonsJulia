54a25e9ea6a0c3521cf134883e8646f5982529a1
Miguel Ayala <miguel.guadarramaayala@mail.mcgill.ca>
2022-12-27 22:06:36 -0500

    Update

--- Soliton2000paper.jl
+++ Soliton2000paper.jl
@@ -7,34 +7,34 @@ include("vectorField2000!.jl")
 
 #### Testing shooting
    
-#    u0 = [ 0.71; 0;  1; 0]
-#    tspan = (0, 50)
-#    prob = ODEProblem(vectorField2000!,u0,tspan)
+   u0 = [ 0.7122079693489833; 0;  1; 0]
+   tspan = (0, 80)
+   prob = ODEProblem(vectorField2000!,u0,tspan)
 
-#    #sol = solve(prob, VCABM(),abstol = 1e-13, reltol = 1e-13)
-#    sol = solve(prob, VCABM(),abstol = 1e-13, reltol = 1e-13)
+   #sol = solve(prob, VCABM(),abstol = 1e-13, reltol = 1e-13)
+   sol = solve(prob, VCABM(),abstol = 1e-13, reltol = 1e-13)
 
-#    # Using the plot recipe tools defined on the plotting page, we can choose to do a 3D phase space plot between the different variables:
+   # Using the plot recipe tools defined on the plotting page, we can choose to do a 3D phase space plot between the different variables:
    
-   #plot(sol,idxs=(0,1))
+   plot(sol,idxs=(0,1))
 
-   N=150
+#    N=150
 
-   initial_conditions = range(.69, stop=.72, length=N)
-   tspan = (0, 50)
-   candidates=[]
+#    initial_conditions = range(0.7122079693489833, stop=0.712207978418047, length=N)
+#    tspan = (0, 50)
+#    candidates=[]
 
-   for  i in 1:N
+#    for  i in 1:N
 
-    u0 = [initial_conditions[i]; 0;  1; 0]
+#     u0 = [initial_conditions[i]; 0;  1; 0]
 
-    prob = ODEProblem(vectorField2000!,u0,tspan)
-    sol = solve(prob, Tsit5(),abstol = 1e-13, reltol = 1e-13)
+#     prob = ODEProblem(vectorField2000!,u0,tspan)
+#     sol = solve(prob, Tsit5(),abstol = 1e-13, reltol = 1e-13)
 
-    println(i)
-    println(sol[end][1])
-    if abs(sol[end][1]) < 1e-5
-        push!(u0 , candidates)
-    end    
+#     println(i)
+#      println(sol[end][1])
+#     if abs(sol[end][1]) < 1e-6
+#         push!(candidates, u0)
+#     end    
     
-   end
+#    end
